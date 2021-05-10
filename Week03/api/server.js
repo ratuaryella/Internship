0930faff95c102d9bd6c2911877cbaf9aa08542f
Kevin Martua A <kevin.aruan00@gmail.com>
2021-05-10 19:21:04 +0700

    Delete and Server

---
+++ Week03/api/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./models");
+// db.sequelize.sync();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+  };
+
+app.use(cors(corsOptions));
+
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Server Connected" });
+  });
+
+
+
+require ("./routes/routes.js")(app);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
