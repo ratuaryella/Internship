1ae2ea4225c199a8d5aaffc974d9cde15674e04b
sarahchrstne <sarah.christinee20@gmail.com>
2021-05-10 19:14:43 +0700

    Menambah POST, DELETE dan routes

--- Week03/api/controller/function.js
+++ Week03/api/controller/function.js
@@ -31,6 +31,47 @@ exports.findOne = (req, res) => {
         message: "Error retrieving Issue with id=" + id
       });
     });
+
+    exports.delete = (req, res) => {
+      const id = req.params.id;
+  
+      Issue.destroy({
+        where: { id: id }
+      })
+        .then(num => {
+          if (num == 1) {
+            res.send({
+              message: "Data deleted"
+            });
+          } else {
+            res.send({
+              message: `Cannot delete Issue with id=${id}.`
+            });
+          }
+        })
+        .catch(err => {
+          res.status(500).send({
+            message: "Could not delete Issue with id=" + id
+          });
+        });
+  };
+  
+  exports.deleteAll = (req, res) => {
+      Issue.destroy({
+          where: {},
+          truncate: false
+        })
+          .then(nums => {
+            res.send({ message: `${nums} Data deleted` });
+          })
+          .catch(err => {
+            res.status(500).send({
+              message:
+                err.message || "Error occured while removing all data"
+            });
+          });
+  };
+  
 };
 
 
