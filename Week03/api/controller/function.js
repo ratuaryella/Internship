92cbe95a4e447dfb04453ef94717b48ddd94d151
sarahchrstne <sarah.christinee20@gmail.com>
2021-05-10 19:18:40 +0700

    Memasukkan POST, PUT dan routes

--- Week03/api/controller/function.js
+++ Week03/api/controller/function.js
@@ -32,46 +32,57 @@ exports.findOne = (req, res) => {
       });
     });
 
-    exports.delete = (req, res) => {
+    exports.create = (req, res) => {
+      if(!req.body.label){
+          res.status(400).send({
+              message:"Content can not be empty!"
+          });
+          return;
+      }
+  
+      const issue ={
+          label: req.body.label,
+          status: req.body.status,
+          priority: req.body.priority
+      };
+  
+      Issue.create(issue)
+      .then(data=>{
+          res.send(data);
+      })
+      .catch(err=>{
+          res.status(500).send({
+              message:
+              err.message || "Some error occurred while creating the Issue."
+          })
+      })
+    
+  };
+  
+  exports.update = (req, res) => {
       const id = req.params.id;
   
-      Issue.destroy({
-        where: { id: id }
+      Issue.update(req.body, {
+          where: { id: id }
       })
-        .then(num => {
+          .then(num => {
           if (num == 1) {
-            res.send({
-              message: "Data deleted"
-            });
+              res.send({
+              message: "Updated"
+              });
           } else {
-            res.send({
-              message: `Cannot delete Issue with id=${id}.`
-            });
+              res.send({
+              message: `Cannot update Issue with id=${id}`
+              });
           }
-        })
-        .catch(err => {
-          res.status(500).send({
-            message: "Could not delete Issue with id=" + id
-          });
-        });
-  };
-  
-  exports.deleteAll = (req, res) => {
-      Issue.destroy({
-          where: {},
-          truncate: false
-        })
-          .then(nums => {
-            res.send({ message: `${nums} Data deleted` });
           })
           .catch(err => {
-            res.status(500).send({
-              message:
-                err.message || "Error occured while removing all data"
-            });
+          res.status(500).send({
+              message: "Error updating Issue with id=" + id
           });
+      });
   };
-  
+
 };
 
 
