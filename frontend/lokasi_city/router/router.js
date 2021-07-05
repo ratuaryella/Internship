e5dd38d02a281992fdba8c40c771c9e78f1f3040
Kevin Martua A <kevin.aruan00@gmail.com>
2021-07-05 23:20:34 +0700

    FE Continue

--- frontend/lokasi_city/router/router.js
+++ frontend/lokasi_city/router/router.js
@@ -7,6 +7,33 @@ router.get('/', (req, res) => {
     res.render('./pages/login/login');
 });
 
+router.get('/home', (req, res) => {
+    res.render('./pages/admin/home');
+});
+
+router.get('/data-user', (req, res) => {
+    res.render('./pages/admin/datauser');
+});
+router.get('/tatanan', (req, res) => {
+    res.render('./pages/admin/tatanan');
+});
+router.get('/kelola-lokasi', (req, res) => {
+    res.render('./pages/admin/lokasi');
+});
+router.get('/kegiatan-petugas', (req, res) => {
+    res.render('./pages/admin/petugaskegiatan');
+});
+router.get('/kegiatan-umum', (req, res) => {
+    res.render('./pages/admin/umumkegiatan');
+});
+
+router.get('/home-petugas', (req, res) => {
+    res.render('./pages/petugas/home_petugas');
+});
+
+router.get('/berita-petugas', (req, res) => {
+    res.render('./pages/petugas/berita_petugas');
+});
 router.get('/maps', mapsController.index);
 
 module.exports = router;
