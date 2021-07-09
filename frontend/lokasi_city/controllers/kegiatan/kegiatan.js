3b806601a17d729282bbc7dcacd4156fd20e52ec
Amzesmoro Sianturi <amzesmoro05@gmail.com>
2021-07-09 19:58:10 +0700

    fetch kegiatan

---
+++ frontend/lokasi_city/controllers/kegiatan/kegiatan.js
@@ -0,0 +1,10 @@
+const fetchKegiatan = require('../../fetch/kegiatan/kegiatan');
+
+const getAllKegiatan = async (req, res, next) => {
+  // return await fetchKegiatan.getAllKegiatan(req)
+  // .then()
+}
+
+module.exports = {
+  getAllKegiatan,
+}
