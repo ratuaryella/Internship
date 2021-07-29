23942bb2d606339a517166f59a2f45f84dcf2fd7
Amzesmoro Sianturi <amzesmoro05@gmail.com>
2021-07-29 11:41:22 +0700

    add dropdown filter kecamatan and desa without paging 1st step

--- frontend/lokasi_city/public/js/maps/maps-home.js
+++ frontend/lokasi_city/public/js/maps/maps-home.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(async function () {
   
   let mapCity = new L.map('map-city', {
     zoomControl:false,
@@ -14,4 +14,88 @@ $(document).ready(function () {
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
   }).addTo(mapCity);
+
+});
+
+let dropdownKecamatan = $('#select-kecamatan');
+dropdownKecamatan.html('<option></option>');
+
+let dropdownDesa = $('#select-desa');
+dropdownDesa.html('<option></option>');
+
+dropdownDesa.select2({
+  placeholder: 'Pilih Desa',
+});
+
+dropdownKecamatan.select2({
+  placeholder: 'Pilih Kecamatan',
+	ajax: {
+    url: '/get-kecamatan?kodeprov=12&kodekabkot=75&page=1',
+    dataType: 'json',
+    type: 'GET',
+    delay : 250,
+    data: function (params) {
+      return {        
+        page: params.page
+      };
+    },
+    processResults(data, params) {
+      params.page = params.page || 1;
+      
+      return {
+        results: $.map(data.results, function (item) {
+          return {
+            text: item.namakec,
+            id: item.kodekec,
+          }
+        }),
+        // results: data.results,
+        // pagination: {
+        //   more: params.page < parseInt(data.total)
+        // }
+      }
+    },
+  },
+});
+
+dropdownKecamatan.on('change', function() {
+  let valueKecSelected = $('#select-kecamatan').val();
+  dropdownDesa.select2({
+    placeholder: 'Pilih Desa',
+    ajax: {
+      url: `/get-desa?kodeprov=12&kodekabkot=75&kodekec=${valueKecSelected}&page=1`,
+      dataType: 'json',
+      type: 'GET',
+      delay : 250,
+      data: function (params) {
+        return {        
+          page: params.page
+        };
+      },
+      processResults(data, params) {
+        params.page = params.page || 1;
+        
+        return {
+          results: $.map(data.results, function (item) {
+            return {
+              text: item.namadesa,
+              id: item.kodedesa,
+            }
+          }),
+          // results: data.results,
+          // pagination: {
+          //   more: params.page < parseInt(data.total)
+          // }
+        }
+      },
+    },
+  });
+});
+
+
+$('#btn-search-poi').on('click', function() {
+  let idKec = $('#select-kecamatan').val();
+  console.log("🚀 ~ file: maps-home.js ~ line 100 ~ $ ~ idKec", idKec)
+  let idDesa = $('#select-desa').val();
+  console.log("🚀 ~ file: maps-home.js ~ line 102 ~ $ ~ idDesa", idDesa)
 });
